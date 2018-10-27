9d8858e35e8a5d2b623236e586f001d45bf7b6b1
Arthur Gomes <arthur.mirandag@gmail.com>
2018-10-26 23:36:07 -0300

    Updates

---
+++ precache-manifest.d988818424063d5e613e896c9c5affcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aeca531dd147ec40063",
+    "url": "/tp-01/static/js/runtime~main.1aeca531.js"
+  },
+  {
+    "revision": "e947341b5b1f5728d4ac",
+    "url": "/tp-01/static/js/main.e947341b.chunk.js"
+  },
+  {
+    "revision": "16651ecd0c0a8da57570",
+    "url": "/tp-01/static/js/1.16651ecd.chunk.js"
+  },
+  {
+    "revision": "e947341b5b1f5728d4ac",
+    "url": "/tp-01/static/css/main.961b0d51.chunk.css"
+  },
+  {
+    "revision": "b1fcbac6c45f68525a1945aad98fcfd8",
+    "url": "/tp-01/index.html"
+  }
+];
